41c0e7f533d5da7d84fe7c0f4de128fd663e7351
AnjinIsmail <anjin.ismail@gmail.com>
2021-02-26 14:35:57 -0800

    mutations added

--- server/server.js
+++ server/server.js
@@ -1,22 +1,20 @@
 const express = require('express');
-//import ApolloServer
-const {ApolloServer} = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
-//import our typeDesfs and resolvers
-const {typeDefs, resolvers} = require ('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-//create a new Apollo server and pass in our schema data 
 const server = new ApolloServer({
-typeDefs,
-resolvers
+  typeDefs,
+  resolvers,
+  context: authMiddleware
 });
 
-//integrate our Apollow server with the express application as middleware 
-server.applyMiddleware({app});
-
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -24,7 +22,6 @@ app.use(express.json());
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    //log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
